c5a8f8a99cd149b926b433b1035e39d11a8e1c4c
ivanvolikaren <ivanvolikaren@gmail.com>
2026-09-20 03:18:06 -0500

    Create settings.gradle.kts

---
+++ settings.gradle.kts
@@ -0,0 +1,17 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "Wild Rift Pro Companion"
+include(":app")
